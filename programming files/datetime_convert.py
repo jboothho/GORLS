c4f73060f805c73a1b89145276e5c9a37b0ac6a7
AlexaWitkin <143548830+AlexaWitkin@users.noreply.github.com>
2024-04-11 16:50:28 -0400

    added comments to datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,7 +1,13 @@
 from datetime import datetime
 
+# initialize the date as a string
 date_str = "2022-03-17 10:45:30"
+
+# create a date object 
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+# create a formatted date of the date object 
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
 
+# display the formatted date to user
 print(formatted_date)
