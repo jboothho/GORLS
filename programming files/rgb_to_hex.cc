b8f572c0843f760b63fb1a5543afe1b3429bc132
cbutera15 <85854580+cbutera15@users.noreply.github.com>
2024-04-11 17:02:12 -0400

    Update rgb_to_hex.cc
    hahAHAHAHHAHA

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -8,8 +8,8 @@ std::string rgb_to_hex(int r, int g, int b)
     g = std::max(0, std::min(255, g));
     b = std::max(0, std::min(255, b));
 
-    std::stringstream ss;
-    ss << std::uppercase << std::hex << std::setfill('0')
+    stringstream sss;
+    ss << std::uppercase << hex << std::setfill('0')
        << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
 
     return ss.str();
